068a37fe3afdc4740fdec2e73d9886798e498849
StevenNZ <listevennz@gmail.com>
2023-07-19 20:02:32 +1200

    feat: installed tailwindcss

--- software-development/frontend/src/App.tsx
+++ software-development/frontend/src/App.tsx
@@ -1,18 +1,11 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import { ThemeProvider } from "@emotion/react";
-import { lightTheme } from "./themes";
-import { CircularProgress } from "@mui/material";
 
 function App() {
   return (
     //TODO: Hook theming up to redux so that it selects
-    <ThemeProvider theme={lightTheme}>
-      <div className="App">
-        <p>hi</p>
-      </div>
-    </ThemeProvider>
+    <div className="App">
+      <p>hi</p>
+    </div>
   );
 }
 
