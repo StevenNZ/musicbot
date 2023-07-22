2cd1320cbf4c8bd14a3b282bf70222e37aba51e2
StevenNZ <listevennz@gmail.com>
2023-07-22 21:26:08 +1200

    refactor: hero height adjusted to screen size

--- software-development/frontend/src/components/Hero.tsx
+++ software-development/frontend/src/components/Hero.tsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <section className="text-white pt-[100px] banner-image lg:h-screen">
+    <section className="text-white pt-[100px] banner-image h-screen">
       <div className="w-10/12 lg:w-3/4 mx-auto">
         <div
           className="min-h-[400px] flex flex-col text-center sm:min-h-[600px] lg:max-w-[75%] lg:text-left lg:w-[500px]"
           title="Workout Equipment image by Rochak Shukla on Freepik"
         >
-          <h1 className=" text-xl sm:text-4xl lg:text-7xl lg:pt-20 lg:pb-16 font-bold pb-6">
+          <h1 className=" text-3xl sm:text-4xl lg:text-7xl pt-20 pb-16 font-bold">
             Discover, Customize, and Conquer Your Fitness Journey!
           </h1>
           <button className="btn primary-colour-bg w-fit mx-auto text-xl lg:text-4xl lg:ml-0">
