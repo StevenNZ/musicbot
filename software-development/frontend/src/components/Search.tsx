bf5f8a1fe9eca5edd32865337328d44b6019eda0
StevenNZ <listevennz@gmail.com>
2023-07-23 18:00:27 +1200

    feat: fetch exercice api initialised

--- software-development/frontend/src/components/Search.tsx
+++ software-development/frontend/src/components/Search.tsx
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Search = () => {
+  const [search, setSearch] = useState("");
+
+  //   const handleSearch = async () => {
+  //         if (search)
+  //   }
+
   return (
     <section className="py-20">
       <div className="w-10/12 lg:w-3/4 mx-auto">
@@ -10,11 +16,17 @@ const Search = () => {
         <div className="flex justify-center">
           <input
             type="text"
-            id="large-input"
             className="block w-8/12 max-w-[1000px] p-4 text-gray-900 border border-gray-300 rounded-xl bg-gray-50 rounded-r-none"
             placeholder="Search an exercise"
+            value={search}
+            onChange={(value) => {
+              setSearch(value.target.value);
+            }}
           ></input>
-          <button className="primary-colour-bg rounded-l-none lg:px-12">
+          <button
+            className="primary-colour-bg rounded-l-none lg:px-12"
+            //  onClick={handleSearch}
+          >
             Search
           </button>
         </div>
